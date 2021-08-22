ebb4a2c4c069db0b95e1b3c736f7080710089302
Satria Rahmat <64813392+Nux-xader@users.noreply.github.com>
2021-08-22 14:10:16 +0700

    add readme

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 // Change 1000 to any number
 const num = 1000;
-const event = new KeyboardEvent('keydown', {
-	key: 'g',
-	ctrlKey: true
-});
+const event = new KeyboardEvent('keydown', {key: ' ', ctrlKey: true});
 setInterval(function(){
-	for (i = 0; i < num; i++) {
-		document.dispatchEvent(event);
-	}
+	for (i=0; i < num; i++) { document.dispatchEvent(event)}
 }, 0);
