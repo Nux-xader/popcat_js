813cf3ca2eeff19b592a568ee084964da7e84c3e
Nux-xader <sumgmng@gmail.com>
2021-08-22 13:57:17 +0700

    gabut

---
+++ index.js
@@ -0,0 +1,11 @@
+// Change 1000 to any number
+const num = 1000;
+const event = new KeyboardEvent('keydown', {
+	key: 'g',
+	ctrlKey: true
+});
+setInterval(function(){
+	for (i = 0; i < num; i++) {
+		document.dispatchEvent(event);
+	}
+}, 0);
