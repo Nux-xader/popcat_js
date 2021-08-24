2e3aa07df613c7305712e31b97b18b218dc26ec4
AlFathir <46089610+alfathir@users.noreply.github.com>
2021-08-24 07:31:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Change 1000 to any number
-const num = 1000;
-const event = new KeyboardEvent('keydown', {key: ' ', ctrlKey: true});
+var num = 1000;
+var event = new KeyboardEvent('keydown', {key: ' ', ctrlKey: true});
 for (i=0; i < num; i++) { document.dispatchEvent(event)}
