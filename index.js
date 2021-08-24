0b8a0ceec6842a796968c48573d1ffe62e63d843
AlFathir <46089610+alfathir@users.noreply.github.com>
2021-08-24 07:26:27 +0700

    Update index.js
    Cleaning code.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Change 1000 to any number
 const num = 1000;
 const event = new KeyboardEvent('keydown', {key: ' ', ctrlKey: true});
-setInterval(function(){
-	for (i=0; i < num; i++) { document.dispatchEvent(event)}
-}, 0);
+for (i=0; i < num; i++) { document.dispatchEvent(event)}
